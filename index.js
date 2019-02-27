3e8478e9cda555b52cc28e92536a6bec0a8566e4
aherlihy <anna.herlihy@10gen.com>
2019-02-27 17:10:21 +0100

    Add collection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var CollectionModel = require('./lib/collection-model');
 var ExtendedCollectionModel = require('./lib/extended-collection-model');
+var ExtendedCollectionModelCollection = require('./lib/extended-collection-model').Collection;
 var ScopeClientMixin = require('./lib/mongodb-scope-client-mixin');
 
 module.export = CollectionModel;
 module.export.ExtendedCollectionModel = ExtendedCollectionModel;
 module.export.ScopeClientMixin = ScopeClientMixin;
+module.export.Collection = ExtendedCollectionModelCollection;
